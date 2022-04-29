af69c69b56e9fa8e5a831eda46bf10f1c8e08036
kt5635 <89210763+kt5635@users.noreply.github.com>
2022-04-28 21:13:23 -0600

    v.

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.getElementById("case-study-button").onclick = function () {
+  myFunction();
+};
+function myFunction() {
+  document.getElementById("case-study-button").index.html = "link";
+}
